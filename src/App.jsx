d8e53e078ebdcc300209752347f4de4787af1077
Cuaquira <angelcuaquirabaltazar@gmail.com>
2022-08-06 14:06:47 -0500

    se modifico app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { HashRouter, Routes, Route, } from 'react-router-dom'
 import './App.css'
 import { Home, Login, ProductDetail, Purchases } from "./Pages"
-import { NavBar, LoadingScreen } from "./Components";
-import { useSelector } from "react-redux";
+import { NavBar, LoadingScreen } from "./Components"
+import { useSelector } from "react-redux"
 import {Container} from "react-bootstrap"
 
   function App() {
