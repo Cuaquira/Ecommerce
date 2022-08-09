e2977e4bca23f1ff7255857f964ee82005b68443
Cuaquira <angelcuaquirabaltazar@gmail.com>
2022-08-09 07:20:00 -0500

    categories

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { getFilterThunk, getProductThunk } from '../store/slices/productSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { Row, Col, Card, InputGroup, Form, Button } from 'react-bootstrap';
+import { Row, Col, Card, InputGroup, Form, Button, ListGroup } from 'react-bootstrap';
+import axios from 'axios';
 
 
 
@@ -14,42 +15,69 @@ const Home = () => {
     const product = useSelector(state => state.product)
 
     const [searchProduct, setSearchProduct] = useState("");
+    const [category, setCategory] = useState([]);
 
     useEffect(() => {
         dispatch(getProductThunk())
-    }, [])
+
+        axios.get("https://ecommerce-api-react.herokuapp.com/api/v1/products/categories/")
+            .then(res => setCategory(res.data.data))
+    }, []);
 
 
+    console.log(product)
     return (
         <div>
-            <h1>Home</h1>
-            <InputGroup className="mb-3">
-                <Form.Control
-                    placeholder="Recipient's username"
-                    aria-label="Recipient's username"
-                    aria-describedby="basic-addon2"
-                    onChange={e => setSearchProduct(e.target.value)}
-                    value={searchProduct}
-                />
-                <Button variant="outline-secondary" onClick={() => dispatch(getFilterThunk(searchProduct))}>
-                    Button
-                </Button>
-            </InputGroup>
 
-            <Row xs={1} md={2} lg={3} className="g-4">
-                {product.products?.map(productItem => (
-                    <Col key={productItem.id}>
-                        <Card onClick={() => navigate(`/shop/${productItem.id}`)}>
-                            <Card.Img variant="top" src={productItem.productImgs} />
-                            <Card.Body>
-                                <Card.Title>{productItem.title}</Card.Title>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                ))}
+            <Row>
+                <Col lg={3}>
+
+                    <ListGroup>
+                        {
+                            category.categories?.map(categories => (
+                                <ListGroup.Item key={categories.id}>{categories.name}</ListGroup.Item>
+
+                            ))
+                        }
+
+                    </ListGroup>
+                </Col>
+
+                <Col>
+                    <h1>Home</h1>
+                    <InputGroup className="mb-3">
+                        <Form.Control
+                            placeholder="Recipient's username"
+                            aria-label="Recipient's username"
+                            aria-describedby="basic-addon2"
+                            onChange={e => setSearchProduct(e.target.value)}
+                            value={searchProduct}
+                        />
+                        <Button variant="outline-secondary" onClick={() => dispatch(getFilterThunk(searchProduct))}>
+                            Button
+                        </Button>
+                    </InputGroup>
+
+                    <Row xs={1} md={2} lg={3} className="g-4">
+                        {product.products?.map(productItem => (
+                            <Col key={productItem.id}>
+                                <Card onClick={() => navigate(`/shop/${productItem.id}`)}>
+                                    <Card.Img variant="top" src={productItem.productImgs} />
+                                    <Card.Body>
+                                        <Card.Title>{productItem.title}</Card.Title>
+                                        <div>
+                                            <Card.Title>{productItem.price}</Card.Title>
+                                            <button><i class="fa-solid fa-cart-shopping"></i></button>
+                                        </div>
+
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        ))}
+                    </Row>
+                </Col>
             </Row>
-
-        </div>
+        </div >
     );
 };
 
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+
+    const { register, handleSubmit} = useForm();
+
+    const submit = data =>{
+        alert("Hice submit")
+        reset(
+            email="",
+            password=""
+        )
+    }
+
+
     return (
         <div>
             <h1>Login</h1>
+            <Form onSubmit={handleSubmit(submit)}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" {...register("email")}/>
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" {...register("password")}/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     );
 };
